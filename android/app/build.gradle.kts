61c5b32107a17ee84791648eb4b8b180c47bbef2
Filipe Jardim <o_teu_email@exemplo.com>
2026-09-01 21:03:42 +0100

    codmagic erro

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.padlock.app"
-    compileSdk = 34 
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -23,7 +23,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         multiDexEnabled = true
-        targetSdk = 34
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
